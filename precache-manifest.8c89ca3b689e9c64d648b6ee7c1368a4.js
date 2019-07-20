84f3c810116c72b5b971b4ff489ce678ddb65bdd
unknown <alfredo.morales@polarissolutions.com>
2019-07-20 09:10:22 -0500

    Updates

---
+++ precache-manifest.8c89ca3b689e9c64d648b6ee7c1368a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fa3d5e5171451eec34d4f6fae22b22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8c0f75209297721944ce",
+    "url": "/monsters-rolodex/static/css/main.26ebe5c4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8c0f75209297721944ce",
+    "url": "/monsters-rolodex/static/js/main.b5daec4b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
